cc79f16b131820398573504e52af28819fe8b818
Santiago Smith <sssilvar@unal.edu.co>
2018-01-05 07:25:05 -0500

    Data science added

---
+++ data-science/01_pandas.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+df = pd.read_csv('librarians-by-msa.csv', index_col=0)
+print(df.head())
+
+# Print one column with all the observations
+print('[INFO]: Printing a column as a Pandas Series')
+data = df['area_name']
+print(data, '\n Type: ', type(data))
+
+# Print two columns as Pandas DataFrame
+print('[INFO]: Printing two columns as a Pandas DataFrame')
+data = df.loc[:, ['area_name', 'tot_emp']]
+print(data, '\n Type: ', type(data))
+
+#
